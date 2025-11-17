95b4e1cf69e5d6d2bb2ffe0da8c2e8cadc0d786a
Whisil <electromandarin@gmail.com>
2025-11-17 10:12:07 +0100

    init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,35 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  {
+    ignores: [".next/**/*"],
+  },
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ),
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      // React 17+ handles JSX runtime automatically in Next.js.
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+]
 
-export default eslintConfig;
+export default eslintConfig
